4155fa617797733cb23bc06c0e277533729b9f99
Brian Jing <brianjing1@gmail.com>
2025-11-14 20:06:10 -0800

    step 1.5

--- map.js
+++ map.js
@@ -10,7 +10,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYnJpYW5qaW5neSIsImEiOiJjbWh6bTloMWEwYmdqMmlvZ
 // Initialize the map
 const map = new mapboxgl.Map({
     container: 'map', // ID of the div where the map will render
-    style: 'mapbox://styles/mapbox/streets-v12', // Map style
+    style: 'mapbox://styles/brianjingy/cmhzrim7a00kh01sublx7ayb5', // Map style
     center: [-71.09415, 42.36027], // [longitude, latitude] --> flipped order
     zoom: 12, // Initial zoom level
     minZoom: 5, // Minimum allowed zoom
